e2085d7d0029d07980e68ae5f01f6f15ab377d7b
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-04-30 18:39:53 -0500

    number buttons created and styled

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,22 @@ export default function Home() {
 			</Head>
 			<div className={styles.container}>
 				<div className={styles.displayValue}>{displayValue}</div>
+				<div className={styles.calcContainer}>
+					<button className={styles.calcButton}>7</button>
+					<button className={styles.calcButton}>8</button>
+					<button className={styles.calcButton}>9</button>
+					<button className={styles.calcButton}>x</button>
+					<button className={styles.calcButton}>4</button>
+					<button className={styles.calcButton}>5</button>
+					<button className={styles.calcButton}>6</button>
+					<button className={styles.calcButton}>-</button>
+					<button className={styles.calcButton}>1</button>
+					<button className={styles.calcButton}>2</button>
+					<button className={styles.calcButton}>3</button>
+					<button className={styles.calcButton}>+</button>
+					<button className={styles.calcButton}>0</button>
+					<button className={styles.calcButton}>=</button>
+				</div>
 			</div>
 		</div>
 	);
